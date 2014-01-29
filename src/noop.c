876fbbe120f9b373c25fae1ed3fe3cee12493a41
Stephen Kell <srk31@cl.cam.ac.uk>
2014-01-29 16:29:18 +0000

    Simulate weak linkage by linking overridable noop implementation. Then use preload linkage by default.

---
+++ src/noop.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+static const int __libcrunch_is_initialized = 1;
+
+int __is_a_internal(const void *obj, const void *r)
+{
+	return 1;
+}
+
+int __libcrunch_global_init(void)
+{
+	return 0;
+}
+
+const void *__libcrunch_typestr_to_uniqtype(const void *r)
+{
+	return NULL;
+}
