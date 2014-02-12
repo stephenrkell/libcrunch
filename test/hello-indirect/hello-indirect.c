14fdc9a2c255406c1d4525347965d30c54a37be8
Stephen Kell <srk31@cl.cam.ac.uk>
2014-02-12 16:42:30 +0000

    Treat indirect callsites as candidate allocation sites.

---
+++ test/hello-indirect/hello-indirect.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+/* Disable optimization to prevent compiler from un-indirecting 
+ * our indirect calls. */
+
+int (__attribute__((optimize("O0"))) main)()
+{
+	void *(*fn)(size_t) = &malloc;
+	
+	int *blah = (int *) fn(200 * sizeof (int));
+	
+	for (int i = 0; i < 200; ++i) blah[i] = 42;
+	
+	void *fake = blah;
+
+	int *recovered = (int *) fake;
+
+	printf("It says: %d\n", recovered[0]);
+
+	free(blah);
+
+	void *(**fn2)(size_t) = &fn;
+	
+	blah = (int *) (*fn2)(200 * sizeof (int));
+	
+	fake = blah;
+	recovered = (int *) fake;
+
+	void *(***fn3)(size_t) = &fn2;
+	
+	blah = (int *) (***fn3)(200 * sizeof (int));
+	fake = blah;
+	recovered = (int *) fake;
+	
+	return 0;
+}
