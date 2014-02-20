d7f760f8d1d13b76d09155f8f70d1faf2b4270c0
Stephen Kell <srk31@cl.cam.ac.uk>
2014-02-20 20:53:05 +0000

    Add lazy typing test case.

---
+++ test/lazy-typing/lazy-typing.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <assert.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include "libcrunch.h"
+
+void *test_void_ptr;
+struct sockaddr test_sockaddr;
+
+int main(void)
+{
+	struct foo {
+		int bar;
+	} **blah = malloc(sizeof (void*));
+	
+	assert(__libcrunch_is_initialized);
+	
+	struct sockaddr_in *p_mysock = malloc(sizeof (struct sockaddr));
+	
+	fprintf(stderr, "Allocated a sockaddr_in at %p\n", p_mysock);
+
+	return 0;
+}
