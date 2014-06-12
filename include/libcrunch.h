4ab87e2ed2b91a3a8db863cd47ec0029963c005b
Stephen Kell <srk31@cl.cam.ac.uk>
2014-06-12 15:58:03 +0100

    Makefile and header fixes to reflect changes in liballocs.

--- include/libcrunch.h
+++ include/libcrunch.h
@@ -16,10 +16,10 @@ int __libcrunch_global_init(void) __attribute__((weak));
 // declare as const void *-returning, to simplify trumptr
 const void *__libcrunch_typestr_to_uniqtype(const char *typestr) __attribute__((weak));
 void *__libcrunch_my_typeobj(void) __attribute__((weak));
-int __is_a_internal(const void *obj, const void *uniqtype) __attribute__((weak));
-int __like_a_internal(const void *obj, const void *uniqtype) __attribute__((weak));
-int __named_a_internal(const void *obj, const void *typestr) __attribute__((weak));
-int __check_args_internal(const void *obj, int nargs, ...) __attribute__((weak));
+int __is_a_internal(const void *obj, const void *uniqtype) __attribute__((weak)) __attribute__((hot));
+int __like_a_internal(const void *obj, const void *uniqtype) __attribute__((weak)) __attribute__((hot));;
+int __named_a_internal(const void *obj, const void *typestr) __attribute__((weak)) __attribute__((hot));;
+int __check_args_internal(const void *obj, int nargs, ...) __attribute__((weak)) __attribute__((hot));;
 
 /* The main public API to libcrunch is through several small functions 
  * which are *always* inlined. NOTE: repeat these in trumptr.ml so that
