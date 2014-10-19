0e812d8a0f76905455797e553aad1e34ce4e0413
Stephen Kell <srk31@cl.cam.ac.uk>
2014-10-19 21:27:09 +0100

    test: fix bug in checks.sh that was causing zero exit status in some failing runs.

--- test/checks.sh
+++ test/checks.sh
@@ -1,25 +1,29 @@
 match () {
     line="$1"
     count_pattern="$2"
-    
+
     input="$(cat)"
-    
-    # if we get no input, we don't fail
+
+    # if we get no input, we fail!
     if [ -z "$input" ]; then
-        exit 0
+        false
+    else
+
+        matched="$( echo "$input" | grep "$line" )"
+        nlines_matched="$( echo "$matched" | wc -l )"
+
+        if ! [ $nlines_matched -eq 1 ]; then
+            echo "Unexpectedly matched $nlines_matched lines" 1>&2
+            exit 1
+        fi
+
+        echo "$matched" | sed 's/.*[^0-9]\([0-9]*\)$/\1/' | grep "$count_pattern" 
+
+	    if [ $? -eq 0 ]; then
+            # only chain if we succeeded
+            echo "$input"
+        else
+            echo "Count for \"$line\" did not match '$count_pattern'" 1>&2; false
+        fi
     fi
-    
-    matched="$( echo "$input" | grep "$line" )"
-    nlines_matched="$( echo "$matched" | wc -l )"
-    
-    if ! [ $nlines_matched -eq 1 ]; then
-        echo "Unexpectedly matched $nlines_matched lines" 1>&2
-        exit 1
-    fi
-    
-    echo "$matched" | sed 's/.*[^0-9]\([0-9]*\)$/\1/' | grep "$count_pattern" || \
-        (echo "Count for \"$line\" did not match '$count_pattern'" 1>&2; false) || exit 1
-    
-    # only chain if we succeeded
-    echo "$input"
 }
