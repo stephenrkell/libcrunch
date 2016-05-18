57b8f7492c51a7ec8225529b15cde35fc871f460
Stephen Kell <srk31@cl.cam.ac.uk>
2016-05-18 17:48:41 +0100

    Actually add the config.mk.

---
+++ config.mk
@@ -0,0 +1,22 @@
+THIS_MAKEFILE := $(lastword $(MAKEFILE_LIST))
+SRCROOT := $(dir $(THIS_MAKEFILE))
+
+include local-config.mk
+
+LIBALLOCS ?= $(realpath $(SRCROOT)/../liballocs)
+LIBCXXGEN ?= $(LIBALLOCS)/contrib/libcxxgen
+LIBDWARFPP ?= $(LIBALLOCS)/contrib/libdwarfpp
+LIBSRK31CXX ?= $(LIBALLOCS)/contrib/libsrk31cxx
+LIBCXXFILENO ?= $(LIBALLOCS)/contrib/libcxxfileno
+
+export CXXFLAGS += \
+-I$(LIBCXXGEN)/include \
+-I$(LIBDWARFPP)/include \
+-I$(LIBSRK31CXX)/include \
+-I$(LIBCXXFILENO)/include
+
+export LDFLAGS += \
+-L$(LIBCXXGEN)/lib -Wl,-rpath,$(LIBCXXGEN)/lib \
+-L$(LIBDWARFPP)/lib -Wl,-rpath,$(LIBDWARFPP)/lib \
+-L$(LIBSRK31CXX)/lib) -Wl,-rpath,$(LIBSRK31CXX)/lib \
+-L$(LIBCXXFILENO)/lib -Wl,-rpath,$(LIBCXXFILENO)/lib
