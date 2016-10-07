ee167e54b7999d4acd33f1ba19721b5a21b6039b
Stephen Kell <srk31@cl.cam.ac.uk>
2016-10-07 14:56:09 +0100

    test: add noquery-bounds-adjuststore test case covering the recent crunchbound fixes.

---
+++ test/noquery-bounds-adjuststore/noquery-bounds-adjuststore.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Check we can adjust a pointer and store it somewhere
+ * without the __store_pointer_nonlocal causing a call to __fetch_bounds_ool. 
+ * This is about picking up the locally cached bounds for the 
+ * __cil_adjexpr_NN temporary. */
+
+int main(void)
+{
+	int x[20] = {  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,
+	              10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+	int *px[20];
+	int **p_xs = px;
+
+	p_xs[0] = &x[0];
+	for (int i = 1; i < 20; ++i)
+	{
+		p_xs[i] = p_xs[i-1] + 1;
+	}
+
+	printf("The 20th integer is: %d\n", *p_xs[19]);
+	return 0;
+}
