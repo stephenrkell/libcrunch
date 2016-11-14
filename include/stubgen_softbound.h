9448271c37f80a0c0246fce7caa1f10a651b2cca
Stephen Kell <srk31@cl.cam.ac.uk>
2016-11-14 17:32:49 +0000

    include/stubgen_softbound.h: fix for cases where the size gets multiplied up rather than being passed in directly.

--- include/stubgen_softbound.h
+++ include/stubgen_softbound.h
@@ -4,8 +4,18 @@
   _Bool __caller_is_inst = __tweak_argument_bounds_cookie(__wrap_## name); \
   int argi = 0;
 
+#define round_down_size(sz, round_to) \
+    ((round_to) * ((sz) / (round_to)))
+extern unsigned long __liballocs_get_alloc_size(const void *obj);
+/* It's wrong to use the size_arg, because e.g. for calloc-alikes, 
+ * that doesn't tell us how *many* of the object are being created.
+ * So ask liballocs how big it is. Round that to a multiple of the
+ * size arg. */
 #define do_ret_p(name) \
-  __libcrunch_bounds_t result_bounds = __make_bounds(real_retval, (char*) real_retval + size_arg_ ## name); \
+  unsigned long result_sz = __liballocs_get_alloc_size(real_retval); \
+  unsigned long result_sz_rounded = round_down_size(result_sz, size_arg_ ## name); \
+  __libcrunch_bounds_t result_bounds = __make_bounds(real_retval, \
+      (char*) real_retval + result_sz_rounded ); \
   __push_local_result_bounds(__caller_is_inst, result_bounds);
 
 #endif
