19e3ef116a8b2566c1558b77c17fbb05d31e8d57
Stephen Kell <srk31@cl.cam.ac.uk>
2017-04-10 19:42:17 +0100

    config.mk cleanups: put CXXFLAGS only where they're actually needed, and hopefully avoid letting TRAP_SYSCALLS get set to the empty strig.

--- config.mk
+++ config.mk
@@ -1,7 +1,4 @@
-THIS_MAKEFILE := $(lastword $(MAKEFILE_LIST))
-SRCROOT := $(dir $(THIS_MAKEFILE))
-
--include local-config.mk
+-include $(dir $(lastword $(MAKEFILE_LIST)))/local-config.mk
 
 LIBALLOCS ?= $(realpath $(SRCROOT)/../liballocs)
 LIBCXXGEN ?= $(LIBALLOCS)/contrib/libcxxgen
@@ -11,16 +8,4 @@ LIBCXXFILENO ?= $(LIBALLOCS)/contrib/libcxxfileno
 LIBDLBIND ?= $(LIBALLOCS)/contrib/libdlbind
 TRAP_SYSCALLS ?= $(LIBALLOCS)/contrib/trap-syscalls
 
-export CXXFLAGS += \
--I$(LIBCXXGEN)/include \
--I$(LIBDWARFPP)/include \
--I$(LIBSRK31CXX)/include \
--I$(LIBCXXFILENO)/include \
--I$(LIBDLBIND)/include \
-
-export LDFLAGS += \
--L$(LIBCXXGEN)/lib -Wl,-rpath,$(LIBCXXGEN)/lib \
--L$(LIBDWARFPP)/lib -Wl,-rpath,$(LIBDWARFPP)/lib \
--L$(LIBSRK31CXX)/lib -Wl,-rpath,$(LIBSRK31CXX)/lib \
--L$(LIBCXXFILENO)/lib -Wl,-rpath,$(LIBCXXFILENO)/lib \
--L$(LIBDLBIND)/lib -Wl,-rpath,$(LIBDLBIND)/lib
+#$(warning at some point TRAP_SYSCALLS is $(TRAP_SYSCALLS))
