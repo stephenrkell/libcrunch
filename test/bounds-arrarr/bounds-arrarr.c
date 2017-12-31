70f4f2f1faaf4e1ee61c04eeef57c9788b1e8c40
Stephen Kell <srk31@cl.cam.ac.uk>
2017-12-31 16:01:27 +0000

    test/bounds-arrarr: try to pick less common array sizes, to help trigger h264ref problem with ungenerated uniqtypes.

--- test/bounds-arrarr/bounds-arrarr.c
+++ test/bounds-arrarr/bounds-arrarr.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int *g(void *p)
 {
@@ -7,14 +8,19 @@ int *g(void *p)
 
 int main(void)
 {
-	int x[2][2][2] = { { { 1, 2 }, { 3, 4 } },
+	int x[13][12][11] = { { { 1, 2 }, { 3, 4 } },
 	                   { { 5, 6 }, { 7, 8 } } };
+
+	int dest[sizeof x / sizeof x[0]];
 
 	/* Here we cast x to void*, spilling an int*-typed alias to the cache.
 	 * It's essential that we collapse the type int[2][2][2] to int[8]
 	 * at this point, because we don't generate uniqtypes of the
 	 * form __uniqtype____ARRn___ARRm___x. */
-	printf("Got back: %d\n", g(x)[3]);
+	printf("Got back: %d\n", g(x)[1]);
+
+	memcpy(dest, x, sizeof x);
+	printf("First element in dest: %d\n", dest[0]);
 	
 	return 0;
 }
