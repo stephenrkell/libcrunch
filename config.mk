012e139032e6925ddf10368f3c5a99cb32cb60f3
Stephen Kell <srk31@cl.cam.ac.uk>
2018-04-07 18:37:34 +0100

    Add librunt config lines and include flags.

--- config.mk
+++ config.mk
@@ -7,3 +7,5 @@ LIBSRK31CXX ?= $(LIBALLOCS)/contrib/libsrk31cxx
 LIBCXXFILENO ?= $(LIBALLOCS)/contrib/libcxxfileno
 LIBDLBIND ?= $(LIBALLOCS)/contrib/libdlbind
 TRAP_SYSCALLS ?= $(LIBALLOCS)/contrib/trap-syscalls
+LIBSYSTRAP ?= $(TRAP_SYSCALLS)/libsystrap
+LIBRUNT ?= $(LIBALLOCS)/contrib/librunt
