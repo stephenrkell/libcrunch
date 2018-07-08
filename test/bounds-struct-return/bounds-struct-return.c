793c07497fd4e7e1ad86142a9beb07fe7e81bfa0
Stephen Kell <srk31@cl.cam.ac.uk>
2018-07-08 23:28:45 +0100

    test/bounds-struct-return: make it more complex, to reveal some more (recently fixed) bugs.

--- test/bounds-struct-return/bounds-struct-return.c
+++ test/bounds-struct-return/bounds-struct-return.c
@@ -5,18 +5,19 @@ struct contains_ptr
 	int *do_not_care;
 	long not_a_ptr;
 	int *p;
+	int *otherptr;
 };
 
 struct contains_ptr callee(int *x)
 {
 	static int is[] = { 69105, 4432 };
-	return (struct contains_ptr) { .do_not_care = x, .not_a_ptr = 42l, .p = is };
+	return (struct contains_ptr) { .do_not_care = x, .not_a_ptr = 42l, .p = is, .otherptr = x };
 }
 
 struct contains_ptr l;
 int main(int argc, char **argv)
 {
 	l = callee(&argc);
-	printf("It says: %d\n", *(l.p + 1));
+	printf("It says: %d; otherptr %p\n", *(l.p + 1), l.otherptr);
 	return 0;
 }
